a3ae0d1a8fab6c19d344787526773bcb3512356b
Isabelola <96275763+Isabelola@users.noreply.github.com>
2022-03-31 09:32:17 -0400

    DS-01

---
+++ DS-01/DS-01/Calculadora.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DS_01
+{
+    /// <summary>
+    /// Lógica interna para Calculadora.xaml
+    /// </summary>
+    public partial class Calculadora : Window
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdicao_Click(object sender, RoutedEventArgs e)
+        {
+            double numeroX = Convert.ToDouble(txtNumX.Text);
+            double numeroY = Convert.ToDouble(txtNumY.Text);
+            double resultado = numeroX + numeroY;
+            txtResultado.Text = numeroX.ToString() + " + " + numeroY.ToString() + " = " + resultado.ToString();
+        }
+
+        private void btSub_Click(object sender, RoutedEventArgs e)
+        {
+            double numeroX = Convert.ToDouble(txtNumX.Text);
+            double numeroY = Convert.ToDouble(txtNumY.Text);
+            double resultado = numeroX - numeroY;
+            txtResultado.Text = numeroX.ToString() + " - " + numeroY.ToString() + " = " + resultado.ToString();
+        }
+
+        private void btMult_Click(object sender, RoutedEventArgs e)
+        {
+            double numeroX = Convert.ToDouble(txtNumX.Text);
+            double numeroY = Convert.ToDouble(txtNumY.Text);
+            double resultado = numeroX * numeroY;
+            txtResultado.Text = numeroX.ToString() + " x " + numeroY.ToString() + " = " + resultado.ToString();
+        }
+
+        private void btDiv_Click(object sender, RoutedEventArgs e)
+        {
+            double numeroX = Convert.ToDouble(txtNumX.Text);
+            double numeroY = Convert.ToDouble(txtNumY.Text);
+            double resultado = numeroX / numeroY;
+            txtResultado.Text = numeroX.ToString() + " / " + numeroY.ToString() + " = " + resultado.ToString();
+        }
+    }
+}
---
+++ DS-01/DS-01/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DS_01
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void txtUsuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtSenha_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btMostrarSenha_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btEsqSenha_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            string senha = pwSenha.Password.ToString();
+            string usuario = txtUsuario.Text;
+            if (senha == "abc" && usuario == "isabelly")
+            {
+                MessageBox.Show("Sucesso!");
+                Calculadora calculadora = new Calculadora();
+                txtUsuario.Clear();
+                pwSenha.Clear();
+                calculadora.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Incorreto, tente novamente!");
+                txtUsuario.Clear();
+                pwSenha.Clear();
+            }
+        }
+
+        private void btCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
